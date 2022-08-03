519caef9471371d25dbdcc441c5491623a1ab1a0
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-03 16:56:13 +0200

    added fill in animation for featured list

--- src/components/Discover-Page/FeaturedGames.js
+++ src/components/Discover-Page/FeaturedGames.js
@@ -1,28 +1,125 @@
-import React from "react";
+import React, { useState } from "react";
 
-const GAME = {
-    name: "Spider-Man",
-    releaseDate: "2022.08.12",
-    posterBig:
-        "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
-    posterSmall:
-        "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
-    logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
-    shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
-    price: "59.99",
-    discount: "",
-    gameOnSale: false,
-    gameFeatured: true,
-    genres: ["Action", "Open World", "Exploration"],
-};
+import { Link } from "react-router-dom";
+
+const DUMMY_GAMES = [
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+];
 
 const FeaturedGames = () => {
+    const [activeListItem, setActiveListItem] = useState(3);
+
     return (
         <div className="featured space-between">
             <div className="featured-poster">
-                <img src={`${GAME.posterBig}`} alt="poster" />
+                <img src={`${DUMMY_GAMES[0].posterBig}`} alt="poster" />
             </div>
-            <div className="featured-list"></div>
+
+            <div className="featured-list">
+                {DUMMY_GAMES.map((game, i) => (
+                    <Link
+                        key={i}
+                        to={"/game"}
+                        className={
+                            activeListItem === i ? "featured-item featured-active featured-fill" : "featured-item"
+                        }
+                    >
+                        <div className="featured-item__image">
+                            <img src={`${game.posterSmall}`} alt="poster" />
+                        </div>
+                        <div className="featured-item__text">{game.name}</div>
+                    </Link>
+                ))}
+            </div>
         </div>
     );
 };
