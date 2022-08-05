532212fb539fef2e2711de2485a3d5c3362deb8a
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-05 20:33:56 +0200

    work on caruosel

---
+++ src/Helpers/DummyGames.js
@@ -0,0 +1,275 @@
+export const DUMMY_CAROUSEL_GAMES = [
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+];
+
+export const DUMMY_GAMES = [
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+    {
+        name: "Marvel's Spider-Man Remastered",
+        releaseDate: "2022.08.12",
+        posterBig:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-desktop-1920x1080-69a70c4fbb0b.jpg?h=1080&resize=1&w=1920",
+        posterSmall:
+            "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-thumb-1200x1600-c51cad22ccfc.jpg?h=1280&resize=1&w=960",
+        logo: "https://cdn2.unrealengine.com/egs-spider-man-remaster-carousel-logo-400x222-322650638558.png",
+        shortDescription: "Venture into the brutal Norse realms and fight to fulfill a deeply personal quest.",
+        price: "59.99",
+        discount: "",
+        gameOnSale: false,
+        gameFeatured: true,
+        genres: ["Action", "Open World", "Exploration"],
+    },
+];
--- src/components/Discover-Page/GameCarousel.js
+++ src/components/Discover-Page/GameCarousel.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
+import { DUMMY_CAROUSEL_GAMES } from "../../Helpers/DummyGames";
+
 const GameCarousel = ({ title }) => {
     return (
         <div className="carousel center-column">
@@ -17,7 +19,17 @@ const GameCarousel = ({ title }) => {
                 </div>
             </div>
 
-            <div className="carousel-bottom"></div>
+            <div className="carousel-bottom">
+                {DUMMY_CAROUSEL_GAMES.map((game) => (
+                    <div className="carousel-item">
+                        <div className="carousel-inner">
+                            <div className="carousel-item__image">
+                                <img src={game.posterSmall} alt="poster" />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
