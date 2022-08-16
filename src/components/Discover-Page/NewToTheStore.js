678872a9119d7117ff1625d675b49a4642bd4eb0
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-16 15:00:48 +0200

    basic styling finished

--- src/components/Discover-Page/NewToTheStore.js
+++ src/components/Discover-Page/NewToTheStore.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+import { DUMMY_CAROUSEL_GAMES } from "../../Helpers/DummyGames";
+
+//TODO add cover & wishlist button
+//TODO fix pricing
 const NewToTheStore = () => {
-    return <div></div>;
+    return (
+        <div className="new-to-store center-column">
+            <div className="new-to-store-top">New To The Store</div>
+            <div className="new-to-store-bottom space-between">
+                {DUMMY_CAROUSEL_GAMES.slice(0, 5).map((game, i) => (
+                    <div key={i} className="new-to-store-item">
+                        <div className="new-to-store-item__image">
+                            <img src={game.posterSmall} alt={`${game.name} poster`} />
+                        </div>
+
+                        <div className="new-to-store-item__info">
+                            <div className="new-to-store-item__info-name">{game.name}</div>
+                            <div className="new-to-store-item__info-tag">
+                                <span>Now on the Store</span>
+                            </div>
+                            <div className="new-to-store-item__info-price">${game.price}</div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default NewToTheStore;
