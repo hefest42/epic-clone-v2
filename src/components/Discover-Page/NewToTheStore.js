d05c753f438845ce264ea72ceffd2a9909608051
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-19 11:07:20 +0200

    fixed NewToTheStore wishlist button issues

--- src/components/Discover-Page/NewToTheStore.js
+++ src/components/Discover-Page/NewToTheStore.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { WishlistButton } from "../UI/CoverAndWishlistButton";
+import GameItem from "../UI/GameItem";
 
 import { DUMMY_CAROUSEL_GAMES } from "../../Helpers/DummyGames";
-import { useState } from "react";
 
 //TODO add cover
 //TODO fix pricing
@@ -15,24 +14,8 @@ const NewToTheStore = () => {
             <div className="new-to-store-top">New To The Store</div>
             <div className="new-to-store-bottom space-between">
                 {DUMMY_CAROUSEL_GAMES.slice(0, 5).map((game, i) => (
-                    <div
-                        key={i}
-                        className="new-to-store-item"
-                        onMouseEnter={() => setShowWishlistIdx(i)}
-                        onMouseLeave={() => setShowWishlistIdx("")}
-                    >
-                        <div className="new-to-store-item__image">
-                            <img src={game.posterSmall} alt={`${game.name} poster`} />
-                            {showWishlistButtonIdx === i && <WishlistButton top="-9" right="-15" />}
-                        </div>
-
-                        <div className="new-to-store-item__info">
-                            <div className="new-to-store-item__info-name">{game.name}</div>
-                            <div className="new-to-store-item__info-tag">
-                                <span>Now on the Store</span>
-                            </div>
-                            <div className="new-to-store-item__info-price">${game.price}</div>
-                        </div>
+                    <div key={i} className="new-to-store-item">
+                        <GameItem game={game} />
                     </div>
                 ))}
             </div>
