fd859416f967009520fe271887ea40c574c5a68d
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-22 14:26:48 +0200

    basic layout for SignUp

--- src/components/Log-In-Sign-Up/SignUp.js
+++ src/components/Log-In-Sign-Up/SignUp.js
@@ -1,7 +1,47 @@
 import React from "react";
 
+import SignUpFormInputs from "./SignUpFormInputs";
+
+import { Link } from "react-router-dom";
+import { SiEpicgames } from "react-icons/si";
+
 const SignUp = () => {
-    return <form className="form center-column"></form>;
+    return (
+        <form className="form center-column">
+            <div className="form-inner">
+                <div className="form-logo center-column">
+                    <Link to="/">
+                        <SiEpicgames />
+                    </Link>
+                    <h3>Sign Up</h3>
+                </div>
+
+                <SignUpFormInputs />
+
+                <div>
+                    <div>
+                        <input type="checkbox" name="" id="" />
+                        <div>I would like to receive news, surveys and special offers from Game Store</div>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="" id="" />
+                        <div>I have read and agree to the terms of service</div>
+                    </div>
+                </div>
+
+                <div>
+                    <button>LOG IN NOW</button>
+                </div>
+
+                <div className="form-link center">
+                    <div>Have an Game Store Account?</div>
+                    <Link to="/log-in">Log In</Link>
+                </div>
+            </div>
+        </form>
+    );
 };
 
 export default SignUp;
+
+// className={emailInputValue && passwordInputValue ? "form-button form-button__active" : "form-button"}
