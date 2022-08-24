d31464d9ff6920614b96d327ddb3c0aa1d9bb0c9
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-24 13:18:14 +0200

    added remove/add to cart button css

--- src/components/Wishlist/WishlistItem.js
+++ src/components/Wishlist/WishlistItem.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import { DUMMY_CAROUSEL_GAMES } from "../../Helpers/DummyGames";
 
 import { AiFillWindows } from "react-icons/ai";
@@ -7,24 +9,38 @@ import { AiFillWindows } from "react-icons/ai";
 const WishlistItem = () => {
     const GAME = DUMMY_CAROUSEL_GAMES[0];
 
+    const addToCartHandler = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        console.log("CLICK");
+    };
+
     return (
-        <div className="wishlist-item center">
+        <Link to="/store" className="wishlist-item center">
             <div className="wishlist-item__inner space-between">
                 <div className="wishlist-item__image center">
                     <img src={GAME.posterSmall} alt={`${GAME.name} poster`} />
                 </div>
                 <div className="wishlist-item__info space-between-column">
                     <div className="wishlist-item__info-container space-between">
-                        <div>{GAME.name}</div>
-                        <div>{GAME.price}</div>
+                        <div className="wishlist-item__info-name">{GAME.name}</div>
+                        <div className="wishlist-item__info-price">{GAME.price}</div>
                     </div>
                     <div className="wishlist-item__info-container space-between">
-                        <div></div>
-                        <div></div>
+                        <div>
+                            <AiFillWindows />
+                        </div>
+                        <div>
+                            <button className="wishlist-item__info-remove">Remove</button>
+                            <button className="wishlist-item__info-cart" onClick={addToCartHandler}>
+                                ADD TO CART
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
