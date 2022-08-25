02c934034ed9884223918edacc2abed62630fab5
Nemanja Vidovic <hefest93@yahoo.com>
2022-08-25 15:18:15 +0200

    cart css done

--- src/components/Wishlist-Cart/Cart/Cart.js
+++ src/components/Wishlist-Cart/Cart/Cart.js
@@ -9,8 +9,33 @@ const Cart = () => {
                 <div className="cart-title">My Cart</div>
 
                 <CartItem />
+                <CartItem />
+                <CartItem />
             </div>
-            <div className="cart-right"></div>
+            <div className="cart-right">
+                <div className="cart-summary">
+                    <div className="cart-summary__title">Games Summary</div>
+                    <div className="cart-summary__top">
+                        <div className="cart-summary__container space-between">
+                            <div>Price</div>
+                            <div>$59.99</div>
+                        </div>
+                        <div className="cart-summary__container space-between">
+                            <div>Taxes</div>
+                            <div>
+                                <span>Calculated at Checkout</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cart-summary__bottom">
+                        <div className="cart-summary__container space-between">
+                            <div>Subtotal</div>
+                            <div>$59.99</div>
+                        </div>
+                        <button>CHECK OUT</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
