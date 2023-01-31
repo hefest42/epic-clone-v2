c9ac969bd4ad90d85cc2b44002c394fe8c63464b
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-01-31 16:55:31 +0100

    bug fix with wishlist being empty

--- src/components/Wishlist-Cart/Wishlist/WishlistContainer.js
+++ src/components/Wishlist-Cart/Wishlist/WishlistContainer.js
@@ -4,17 +4,24 @@ import PageContainer from "../../UI/PageContainer";
 import Wishlist from "./Wishlist";
 import WishlistEmpty from "./WishlistEmpty";
 
+import { useNavigate } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 
 const WishlistContainer = () => {
+    const navigate = useNavigate();
     const isAccountLoggedIn = useSelector((state) => state.account.isAccountLoggedIn);
     const loggedInAccount = useSelector((state) => state.account.account);
     const [wishlist, setWishlist] = useState([]);
 
     useEffect(() => {
-        if (!isAccountLoggedIn) return;
+        if (!isAccountLoggedIn) {
+            navigate("/log-in");
+            return;
+        }
 
-        setWishlist(loggedInAccount.wishlist);
+        const accountWishlist = loggedInAccount.wishlist ? loggedInAccount.wishlist : [];
+        setWishlist(accountWishlist);
     }, [loggedInAccount.wishlist, isAccountLoggedIn]);
 
     return <PageContainer>{wishlist.length === 0 ? <WishlistEmpty /> : <Wishlist />}</PageContainer>;
