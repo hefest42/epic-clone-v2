1ee5240a9f8210a4558d918ad6e40f4e0f8e0b95
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-01 16:18:57 +0100

    changed inputs in the modal

--- src/components/Account-Settings/ChangeDisplayNameModal.js
+++ src/components/Account-Settings/ChangeDisplayNameModal.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import Input from "../UI/Input";
+
 import { AiOutlineClose } from "react-icons/ai";
 import { SiEpicgames } from "react-icons/si";
 import { BiErrorCircle } from "react-icons/bi";
@@ -15,38 +17,33 @@ const ChangeDisplayNameModal = ({ setShowDisplayNameModal }) => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const account = useSelector((state) => state.account.account);
+    const [newDisplayName, setNewDisplayName] = useState("");
+    const [confirmNewDisplayName, setConfirmNewDisplayName] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
-    const [isDisplayNameActive, setIsDisplayNameActive] = useState(false);
-    const [displayNameValue, setDisplayNameValue] = useState("");
-    const [isConfirmDisplayNameActive, setIsConfirmDisplayNameActive] = useState(false);
-    const [confirmDisplayNameValue, setConfirmDisplayNameValue] = useState("");
 
     const displayNameChangeHandler = async (e) => {
         e.preventDefault();
 
-        const newDisplayName = displayNameValue;
-        const confirmNewDisplayName = confirmDisplayNameValue;
-
-        if (newDisplayName !== confirmNewDisplayName) {
+        if (false) {
             setErrorMessage("Please check if the input fields match.");
             return;
         }
 
-        try {
-            const response = fetch(`${API_URL}/accounts/${account.accountId}.json`, {
-                method: "PATCH",
-                body: JSON.stringify({ displayName: confirmDisplayNameValue }),
-                headers: {
-                    "CONTENT-TYPE": "application/json",
-                },
-            });
-        } catch (error) {
-            console.error(error);
-            setErrorMessage("Oops.. Something went wrong. Please wait a bit and try again.");
-        }
+        // try {
+        //     const response = fetch(`${API_URL}/accounts/${account.accountId}.json`, {
+        //         method: "PATCH",
+        //         body: JSON.stringify({ displayName: confirmDisplayNameValue }),
+        //         headers: {
+        //             "CONTENT-TYPE": "application/json",
+        //         },
+        //     });
+        // } catch (error) {
+        //     console.error(error);
+        //     setErrorMessage("Oops.. Something went wrong. Please wait a bit and try again.");
+        // }
 
-        dispatch(logOutAccount());
-        navigate("/log-in");
+        // dispatch(logOutAccount());
+        // navigate("/log-in");
     };
 
     return (
@@ -72,51 +69,29 @@ const ChangeDisplayNameModal = ({ setShowDisplayNameModal }) => {
                     </div>
                 )}
                 <form className="settings-modal__form" onSubmit={displayNameChangeHandler}>
-                    <div
-                        className={
-                            isDisplayNameActive
-                                ? "settings-modal__input settings-modal__input-active"
-                                : "settings-modal__input"
-                        }
-                    >
-                        <div className="settings-modal__input-desc">
-                            <label htmlFor="newName">New Display Name</label>
-                        </div>
+                    <Input
+                        inputType="text"
+                        inputName="New Display Name"
+                        inputId="new-name"
+                        inputValue={newDisplayName}
+                        setInputValue={setNewDisplayName}
+                        autocomplete="no"
+                        theme="light"
+                    />
 
-                        <input
-                            type="text"
-                            id="newName"
-                            name="newName"
-                            onClick={() => setIsDisplayNameActive(true)}
-                            onChange={(e) => setDisplayNameValue(e.target.value)}
-                            onBlur={() => !displayNameValue && setIsDisplayNameActive(false)}
-                            value={displayNameValue}
+                    <div className="mt-1">
+                        <Input
+                            inputType="password"
+                            inputName="Confirm New Display Name"
+                            inputId="confirm-name"
+                            inputValue={confirmNewDisplayName}
+                            setInputValue={setConfirmNewDisplayName}
+                            autocomplete="no"
+                            theme="light"
                         />
                     </div>
 
-                    <div
-                        className={
-                            isConfirmDisplayNameActive
-                                ? "settings-modal__input settings-modal__input-active"
-                                : "settings-modal__input"
-                        }
-                    >
-                        <div className="settings-modal__input-desc">
-                            <label htmlFor="confirmNewName">Confirm Display Name</label>
-                        </div>
-
-                        <input
-                            type="text"
-                            id="confirmNewName"
-                            name="confirmNewName"
-                            onClick={() => setIsConfirmDisplayNameActive(true)}
-                            onChange={(e) => setConfirmDisplayNameValue(e.target.value)}
-                            onBlur={() => !confirmDisplayNameValue && setIsConfirmDisplayNameActive(false)}
-                            value={confirmDisplayNameValue}
-                        />
-                    </div>
-
-                    <div className="space-between">
+                    <div className="space-between mt-1">
                         <button
                             className="settings-modal__buttons settings-modal__cancel"
                             onClick={() => setShowDisplayNameModal(false)}
--- src/components/Account-Settings/ChangeEmailAddressModal.js
+++ src/components/Account-Settings/ChangeEmailAddressModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import LoadingSpinner from "../UI/LoadingSpinner";
+import Input from "../UI/Input";
 
 import { AiOutlineClose } from "react-icons/ai";
 import { SiEpicgames } from "react-icons/si";
@@ -18,9 +19,7 @@ const ChangeEmailAddressModal = ({ setShowChangeEmailAddressModal }) => {
     const dispatch = useDispatch();
     const account = useSelector((state) => state.account.account);
     const [stepper, setStepper] = useState(1);
-    const [isPasswordActive, setIsPasswordActive] = useState(false);
     const [passwordValue, setPasswordValue] = useState("");
-    const [isEmailActive, setIsEmailActive] = useState(false);
     const [emailValue, setEmailValue] = useState("");
     const [errorMessage, setErrorMessage] = useState("");
 
@@ -100,30 +99,17 @@ const ChangeEmailAddressModal = ({ setShowChangeEmailAddressModal }) => {
                             </div>
 
                             <form onSubmit={confirmPasswordHandler} className="email-modal__form">
-                                <div
-                                    className={
-                                        isPasswordActive
-                                            ? "settings-modal__input settings-modal__input-active"
-                                            : "settings-modal__input"
-                                    }
-                                >
-                                    <div className="settings-modal__input-desc">
-                                        <label htmlFor="password">Your Password</label>
-                                    </div>
+                                <Input
+                                    inputType="password"
+                                    inputName="Confirm Password"
+                                    inputId="confirm-password"
+                                    inputValue={passwordValue}
+                                    setInputValue={setPasswordValue}
+                                    autocomplete="yes"
+                                    theme="light"
+                                />
 
-                                    <input
-                                        type="password"
-                                        id="password"
-                                        name="password"
-                                        autoComplete="no"
-                                        onClick={() => setIsPasswordActive(true)}
-                                        onChange={(e) => setPasswordValue(e.target.value)}
-                                        onBlur={() => !passwordValue && setIsPasswordActive(false)}
-                                        value={passwordValue}
-                                    />
-                                </div>
-
-                                <div className="email-modal__buttons">
+                                <div className="email-modal__buttons mt-1">
                                     <button className="email-modal__buttons-continue">CONTINUE</button>
                                     <button
                                         className="email-modal__buttons-cancel"
@@ -148,30 +134,19 @@ const ChangeEmailAddressModal = ({ setShowChangeEmailAddressModal }) => {
                             <div className="email-modal__info">Enter your new Email Address</div>
 
                             <form onSubmit={emailChangeHandler} className="email-modal__form">
-                                <div
-                                    className={
-                                        isEmailActive
-                                            ? "settings-modal__input settings-modal__input-active"
-                                            : "settings-modal__input"
-                                    }
-                                >
-                                    <div className="settings-modal__input-desc">
-                                        <label htmlFor="email">New Email Address</label>
-                                    </div>
-
-                                    <input
-                                        type="email"
-                                        id="email"
-                                        name="email"
-                                        autoComplete="no"
-                                        onClick={() => setIsEmailActive(true)}
-                                        onChange={(e) => setEmailValue(e.target.value)}
-                                        onBlur={() => !emailValue && setIsEmailActive(false)}
-                                        value={emailValue}
+                                <div>
+                                    <Input
+                                        inputType="email"
+                                        inputName="New Email"
+                                        inputId="new-email"
+                                        inputValue={emailValue}
+                                        setInputValue={setEmailValue}
+                                        autocomplete="yes"
+                                        theme="light"
                                     />
                                 </div>
 
-                                <div className="email-modal__buttons">
+                                <div className="email-modal__buttons mt-1">
                                     <button type="submit" className="email-modal__buttons-continue">
                                         CONFIRM
                                     </button>
