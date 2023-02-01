ce61e4edd5dba86930e81e46e093a8d838588868
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-01 19:55:44 +0100

    fixed issues with css

--- src/components/UI/AccountSettingsInput.js
+++ src/components/UI/AccountSettingsInput.js
@@ -5,10 +5,9 @@ import { FiEdit2 } from "react-icons/fi";
 const AccountSettingsInput = ({ name, isAccountLoggedIn, value, showModal }) => {
     return (
         <div className="account-settings__inputs-container">
-            <div className={"account-settings__input"}>
-                <label htmlFor="">{name}</label>
-                <div className="account-settings__input-filler"></div>
-                <input type="text" defaultValue={isAccountLoggedIn ? value : ""} />
+            <div className="account-settings__input">
+                <div className="account-settings__input-label">{name}</div>
+                <div className="account-settings__input-value">{isAccountLoggedIn ? value : ""}</div>
             </div>
             <button className="account-settings__button" onClick={() => showModal(true)}>
                 <FiEdit2 />
